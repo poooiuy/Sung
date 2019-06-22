1147cedfce44756badaf1ccea6773f4f27a70a81
cssung <poooiuy@hanmail.net>
2019-06-23 05:44:07 +0900

    2018 TCT 기출문제 Commit

--- src/sung/util/Array.java
+++ src/sung/util/Array.java
@@ -1,5 +1,14 @@
 package sung.util;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Array {
+	
+	//	Array to List Example
+	String[] strArr = new String[] {"1","2","3"};
+	List<String> strList = Arrays.asList(strArr);
+	
+	
 
 }
