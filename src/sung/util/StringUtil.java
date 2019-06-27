4d08fb88ca443333dbcfe14b25fbe72749fc1584
cssung <poooiuy@hanmail.net>
2019-06-27 22:55:55 +0900

    .

--- src/sung/util/StringUtil.java
+++ src/sung/util/StringUtil.java
@@ -8,6 +8,29 @@ public class StringUtil {
     }
 	
 	
+	//	몇번째 숫자 구하기
+	public void getNumericValue(String param) {
+		for(int i=0; i < param.length(); i++) {
+			int n1 = Character.getNumericValue(param.charAt(i));
+		}
+	}
+	
+	
+	
+	//	문자열에 중복문자 제거
+	public void isHasValue() {
+		String tmp = "abfgadfaefd";
+		String ret = "";
+		for(int i=0; i<tmp.length();i++){
+			char n = tmp.charAt(i);
+			if(ret.indexOf(n)==-1){
+				ret+=n;
+			}
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println(reverseString("abcdefg"));
