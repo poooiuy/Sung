58607e31fe45b61bd879cdfb7613d134d8e36216
poooi <poooi@200.5.44.155>
2019-07-14 14:59:22 +0900

    test

--- src/sung/util/StringUtil.java
+++ src/sung/util/StringUtil.java
@@ -1,5 +1,11 @@
 package sung.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class StringUtil {
 	
 	//	문자열 뒤집기
@@ -29,11 +35,26 @@ public class StringUtil {
 		}
 	}
 	
+	//	중복횟수 구하기
+    public static void checkDuplicate() {
+    	 
+        String text = "a r b k c d se f g a d f s s f d s ft gh f ws w f v x s g h d h j j k f sd j e wed a d f";
+ 
+        List<String> list = Arrays.asList(text.split(" "));
+ 
+        Set<String> uniqueWords = new HashSet<String>(list);
+        for (String word : uniqueWords) {
+            System.out.println(word + ": " + Collections.frequency(list, word));
+        }
+    }
+	
 	
 	
 	public static void main(String[] args) {
 		
-		System.out.println(reverseString("abcdefg"));
+//		System.out.println(reverseString("abcdefg"));
+		
+		checkDuplicate();
 		
 	}
 
